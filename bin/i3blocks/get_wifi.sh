bcb23bc439e6437b3bf5f4d5afa1bd56dcda8f0b
2toxic <isaqtm@gmail.com>
2018-10-24 05:50:05 +0300

    Improve something

--- bin/i3blocks/get_wifi.sh
+++ bin/i3blocks/get_wifi.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 SSID="$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | head -1 | sed "s/yes://g")"
-if [[ ! -z "$(ip link show | grep wg0)" ]]
+if [[ ! -z "$(ip link show | grep POINTOPOINT)" ]]
 then
-    printf '<span color="#00a000">%s</span> - ' "WG"
+    printf '<span color="#00a050">%s</span>' $(ip link show | grep -Po "(wg[0-9]+)")
 else
-    printf '<span color="#F030F0">%s</span> - ' "PT"
-fi
-if [[ ! -z "$(ip link show | grep "enp0s20u[0-9]")" ]]
-then
-    printf '<span color="#A0A0F0">%s</span>' `ip link show | grep -Po "enp[0-9]s[0-9]+u\K([0-9]+)"`
-elif [[ ! -z $SSID ]]
-then
-    printf '<span color="#50B0B0">%s</span>' "$SSID"
-else
-    echo 'down'
+    if [[ ! -z "$(ip link show | grep "enp0s20u[0-9]")" ]]
+    then
+        printf '<span color="#A0A0F0">%s</span>' `ip link show | grep -Po "(enp[0-9]s[0-9]+u[0-9]+)"`
+    elif [[ ! -z $SSID ]]
+    then
+        printf '<span color="#50B0B0">%s</span>' "$SSID"
+    else
+        printf '<span color="#70A080">%s</span>' "down"
+    fi
 fi
 
