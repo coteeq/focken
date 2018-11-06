51351d82cda6cf29f4aa0a0f07dd9139aaa4673f
2toxic <isaqtm@gmail.com>
2018-11-06 13:05:20 +0300

    Fix network status

--- bin/i3blocks/get_wifi.sh
+++ bin/i3blocks/get_wifi.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
-SSID="$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | head -1 | sed "s/yes://g")"
-if [[ ! -z "$(ip link show | grep POINTOPOINT)" ]]
+
+#SSID="$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | head -1 | sed "s/yes://g")"
+#if [[ ! -z $SSID ]]
+#then
+#     printf '<span color="#50B0B0">%s</span>' "$SSID"
+#fi
+
+if [[ ! -z "$(ip link show | grep enp0)" ]]
 then
-    printf '<span color="#00a050">%s</span>' $(ip link show | grep -Po "(wg[0-9]+)")
-else
-    if [[ ! -z "$(ip link show | grep "enp0s20u[0-9]")" ]]
+    if [[ ! -z "$(ip link show | grep POINTOPOINT)" ]]
     then
-        printf '<span color="#A0A0F0">%s</span>' `ip link show | grep -Po "(enp[0-9]s[0-9]+u[0-9]+)"`
-    elif [[ ! -z $SSID ]]
-    then
-        printf '<span color="#50B0B0">%s</span>' "$SSID"
+        printf '<span color="#00a050">%s</span>' $(ip link show | grep -Po "(wg[0-9]+)")
     else
-        printf '<span color="#70A080">%s</span>' "down"
+        printf '<span color="#A0A0F0">%s</span>' $(ip link show | grep -Po "(enp0s[0-9]+u[0-9]+)")
     fi
+else
+    printf '<span color="#70A080">%s</span>' "down"
 fi
 
