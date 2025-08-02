ac970192a063073f02c875ca18db0308ec7b88bd
coteeq <isaqtm@gmail.com>
2025-08-02 16:36:38 +0300

    larger history

--- omz-bones/history.zsh
+++ omz-bones/history.zsh
@@ -1,7 +1,7 @@
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-[ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
-[ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
+[ "$HISTSIZE" -lt 200000 ] && HISTSIZE=200000
+[ "$SAVEHIST" -lt 100000 ] && SAVEHIST=100000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
@@ -10,3 +10,4 @@ setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data
+# setopt inc_append_history     # append immediately. conflicts with share_history (as of docs)
