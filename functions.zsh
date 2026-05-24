debb5d012131bc60092f0aa7c8c6670395431fac
сoteeq <42947087+coteeq@users.noreply.github.com>
2026-05-24 12:54:35 +0400

    zsh: safe(r) ssh

--- functions.zsh
+++ functions.zsh
@@ -70,3 +70,25 @@ function loc() {
 	curl -sS https://1.1.1.1/cdn-cgi/trace | grep -E -- '(loc|colo)='
 }
 
+function sssh() {
+    local stty_saved response
+    stty_saved=$(stty -g)
+
+    ssh "$@"
+
+    # Disable mouse events
+    printf '\e[?1000l\e[?1002l\e[?1003l\e[?1006l'
+
+    # Query alt screen state (DECRQM)
+    stty "$stty_saved" 2>/dev/null
+    stty -echo raw min 0 time 1
+    printf '\e[?1049$p'
+    IFS= read -r -d 'y' response
+    stty "$stty_saved"
+
+    # Exit alt screen only if active (1$y is active, 2$y is non-active)
+    local pattern='\?1049;1\$'
+    if [[ "$response" =~ $pattern ]]; then
+      tput rmcup
+    fi
+}
